a3713db58c28d7bf2cfc709c22957cf3052213c6
Shivankar Goyal <97345239+shivankargoyal@users.noreply.github.com>
2024-10-10 14:16:47 +0530

    Time: 0 ms (100%), Space: 41.9 MB (32.31%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,7 +1,31 @@
-class Solution {
+class Solution
+{
     public void sortColors(int[] nums)
     {
-        Arrays.sort(nums);
-        
+        int left=0;
+        int right=nums.length-1;
+        int i=0;
+        while(i<=right)
+        {
+            if(nums[i]==0)
+            {
+                int temp=nums[i];
+                nums[i]=nums[left];
+                nums[left]=temp;
+                left++;
+                i++;
+            }
+            else if(nums[i]==2)
+            {
+                int temp=nums[i];
+                nums[i]=nums[right];
+                nums[right]=temp;;
+                right--;
+            }
+            else
+            {
+                i++;
+            }
+        }
     }
 }
