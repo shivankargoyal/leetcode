4e2e8a1ee1b2973faf5855a5a757d9ab8e423fcd
Shivankar Goyal <97345239+shivankargoyal@users.noreply.github.com>
2024-10-16 11:25:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,17 +1,31 @@
-public class Solution
-{
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
     public boolean hasCycle(ListNode head)
     {
-        ListNode fast=head;
-        ListNode slow=head;
-        while(fast!=null && fast.next!=null)
+        if(head==null || head.next==null)
         {
-            fast=fast.next.next;
-            slow=slow.next;
-            if(fast==slow)
+            return false;
+        }
+        ListNode tost=head;
+        ListNode hare=head;
+        while(hare!=null)
+        {
+            if(hare==tost)
             {
                 return true;
             }
+            hare=hare.next;
+            tost=tost.next.next;
         }
         return false;
     }
