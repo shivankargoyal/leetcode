1665f769841a0011771e7657bba4ed5373fe4caa
Shivankar Goyal <97345239+shivankargoyal@users.noreply.github.com>
2025-08-22 14:13:28 +0530

    Time: 3 ms (46.09%), Space: 18.1 MB (34.57%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,8 +1,3 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        q=len(nums)
-        for i,c in enumerate(nums):
-            nums[i]+= q * (nums[c] % q)
-        for i,_ in enumerate(nums):
-            nums[i] //=q
-        return nums
+        return [nums[x] for x in nums]
