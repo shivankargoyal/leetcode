d870f852303b6adb32dadb4d2a1d26dd31161407
Shivankar Goyal <97345239+shivankargoyal@users.noreply.github.com>
2025-09-12 19:05:52 +0530

    Time: 11 ms (57.08%), Space: 17.8 MB (87.97%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        candidates.sort()   # sort to enable early stopping
+        result = []
+
+        def backtrack(start, total, comb):
+            if total == target:
+                result.append(list(comb)) # record valid comb
+                return
+            if total > target:
+                return  # overshoot -> stop exploring path
+            
+            for i in range(start, len(candidates)):
+                comb.append(candidates[i])
+                backtrack(i, total + candidates[i], comb)  # allow reuse
+                comb.pop()
+
+        backtrack(0, 0, [])
+        return result;
